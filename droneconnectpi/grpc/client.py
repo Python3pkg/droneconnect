41862b82fb1242ac369d4437ee3040f7882042aa
cmagnuso <cmagnuso@uvic.ca>
2016-12-17 12:12:37 -0800

    Add files via upload

---
+++ droneconnectpi/grpc/client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import googlemaps
+from datetime import datetime
+
+
+def main():
+    gmaps = googlemaps.Client(key='Add Your Key here')
+
+    # Geocoding an address
+    geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+    # Look up an address with reverse geocoding
+    reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+    # Request directions via public transit
+    now = datetime.now()
+    directions_result = gmaps.directions("Sydney Town Hall",
+                                         "Parramatta, NSW",
+                                         mode="transit",
+                                         departure_time=now)
+    
+if __name__ == '__main__':
+    main()
---
+++ droneconnectpi/methods/parameters.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from dronekit import connect, VehicleMode
+import time
+import collections
+import sys
+
+#Set up option parsing to get connection string
+import argparse  
+parser = argparse.ArgumentParser(description='Print out vehicle state information. Connects to SITL on local PC by default.')
+parser.add_argument('--connect', 
+                   help="vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+
+connection_string = args.connect
+print connection_string
+
+if not connection_string:
+    print "Usage: python parameters.py --connect port (eg. /dev/ttyUSB0 or /dev/ttyACM0)"
+    sys.exit (1)
+
+# Connect to the Vehicle. 
+#   Set `wait_ready=True` to ensure default attributes are populated before `connect()` returns.
+print "\nConnecting to vehicle on: %s" % connection_string
+vehicle = connect(connection_string, wait_ready=True, baud=57600, heartbeat_timeout=120)
+
+# Parameters
+print "\nPrint all parameters (iterate `vehicle.parameters`):"
+
+# Sorts the list of parameters before printing
+od = collections.OrderedDict(sorted(vehicle.parameters.items()))
+for key, value in od.iteritems():
+	print " Key:%s Value:%s" % (key,value)
+
+#Close vehicle object before exiting script
+print "\nClose vehicle object"
+vehicle.close()
+
+print("Completed")
